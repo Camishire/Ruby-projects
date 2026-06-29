d57fc65e436fd385eca1a08a05eb817f448cc773
Camille <kamile10za@gmail.com>
2026-06-29 09:41:26 +0300

    Create stock_picker.rb

---
+++ Stock Picker/stock_picker.rb
@@ -0,0 +1,14 @@
+def stock_picker(days)
+  days_difference = Hash.new(0)
+  for i in days do
+    index=0
+    for y+index in days do
+      ...
+    end
+  end
+
+end
+
+days = gets.chomp
+days = days.chars
+stock_picker(days)
